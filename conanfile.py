f3317046dd523167addaa7ee9de7fc44cb0c870d
Zoltan Gilian <zoltan.gilian@gmail.com>
2017-08-16 22:12:11 +0200

    Use the new CMake interface in build()

--- conanfile.py
+++ conanfile.py
@@ -30,13 +30,16 @@ conan_basic_setup()""")
         tools.replace_in_file("ilmbase-%s/CMakeLists.txt" % self.version, "ADD_SUBDIRECTORY ( ImathTest )", "")
 
     def build(self):
-        cmake = CMake(self.settings)
-        shared = "-DBUILD_SHARED_LIBS=ON" if self.options.shared else "-DBUILD_SHARED_LIBS=OFF"
-        namespace_versioning = "-DNAMESPACE_VERSIONING=ON" if self.options.namespace_versioning else "-DNAMESPACE_VERSIONING=OFF"
-        cmake_flags = [shared, namespace_versioning, "-DCMAKE_INSTALL_PREFIX=install"]
+        cmake = CMake(self)
+        cmake.definitions.update(
+            { "BUILD_SHARED_LIBS": self.options.shared
+            , "NAMESPACE_VERSIONING": self.options.namespace_versioning
+            , "CMAKE_INSTALL_PREFIX": "install"
+            })
 
-        self.run('cmake ilmbase-%s %s %s' % (self.version, ' '.join(cmake_flags), cmake.command_line))
-        self.run("cmake --build . %s" % cmake.build_config)
+        src_dir = "ilmbase-%s" % self.version
+        cmake.configure(source_dir=src_dir)
+        cmake.build()
 
     def package(self):
         self.copy("*.h", dst="include/OpenEXR", src="ilmbase-%s/Half" % self.version, keep_path=False)
